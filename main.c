81d224f8e69fcfbf69c20cdf6bde777a0d91e0dd
Anhgelus Morhtuuzh <anhgelus.morhtuuzh@gmx.com>
2023-01-29 00:38:31 +0100

    feat exo 1

--- main.c
+++ main.c
@@ -1,7 +1,16 @@
 #include <stdio.h>
 
 int main() {
-    printf("Hello, World!\n");
-    
+    /* Exo 1 de zestedesavoir.com */
+    int a,b;
+    printf("Bonjour !");
+    printf("Veuillez saisir le premier nombre : ");
+    scanf("%d", &a);
+    printf("Veuillez saisir le deuxième nombre : ");
+    scanf("%d", &b);
+    printf("%d + %d = %d\n", a, b, a+b);
+    printf("%d - %d = %d\n", a, b, a-b);
+    printf("%d * %d = %d\n", a, b, a*b);
+    printf("%d / %d = %f\n", a, b, a/(double)b);
     return 0;
 }
